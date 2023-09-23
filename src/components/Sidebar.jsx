13dd6dd449a391ba65f62f5e0b0f0a67d7428c92
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-09-22 23:53:38 -0300

    Refactor code into components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,59 @@
-import { Input } from "@nextui-org/react";
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import { Accordion, AccordionItem, Input } from "@nextui-org/react";
 import { RiUser3Fill } from "react-icons/ri";
-import { Button, Tooltip } from "@nextui-org/react";
+import AddButton from "./AddButton";
+import DetailsCard from "./DetailsCard";
 
-function Sidebar({ cvInfo, onCvChange, createSection }) {
+function Sidebar({ cvInfo, onCvChange, pushToSection, onDeleteClick }) {
+  // Create an algorithm for linkedin input, where it makes it appear in format linkedin.com/in/cristobalnpe/,
+  //in this case user should only type 'cristobalnpe' and the rest should  appear by itself in the preview page.
+
+  //Maybe uppercase the first letter of name/lastnames too
+  //Look for how to include
+
+  const sectionAccordionElements = cvInfo.sections.map((section) => (
+    <AccordionItem
+      key={section.title}
+      aria-label={section.title}
+      title={section.title}
+    >
+      {section.details.length === 0 && (
+        <h1 className="text-sm text-center my-4">
+          Agrege nuevas entradas a {section.title}
+        </h1>
+      )}
+      {section.details.map((detail) => (
+        <DetailsCard
+          fromSection={section.title}
+          key={detail.id}
+          {...detail}
+          deleteHandleClick={onDeleteClick}
+        />
+      ))}
+      <div className="flex justify-end mt-3">
+        <AddButton pushToSection={pushToSection} sectionTitle={section.title} />
+      </div>
+    </AccordionItem>
+  ));
+
+  const basicInfoInputElements = Object.keys(cvInfo.basicInfo).map((info) => {
+    const property = cvInfo.basicInfo[info];
+    return (
+      <Input
+        key={info}
+        type={info === "email" ? "email" : "text"}
+        radius="sm"
+        label={property.label}
+        name={info}
+        value={property.value}
+        onChange={onCvChange}
+        className="text-slate-700 mb-2"
+      />
+    );
+  });
+
   return (
-    <nav className=" flex flex-col bg-slate-400 max-w-[25rem] min-w-[20rem] w-1/4 drop-shadow-xl p-4 gap-5 items-center rounded-r-2xl h-full min-h-screen ">
+    <nav className="no-scrollbar scroll-auto flex flex-col bg-gradient-to-tl from-slate-200 to-slate-300 max-w-[25rem] min-w-[20rem] w-1/4 drop-shadow-xl p-4 gap-5 items-center rounded-r-2xl h-full min-h-screen max-h-screen overflow-scroll ">
       <h1 className="text-2xl my-4 font-black tracking-wide ">CVGenerator</h1>
-
       <Accordion>
         <AccordionItem
           key="1"
@@ -15,55 +61,10 @@ function Sidebar({ cvInfo, onCvChange, createSection }) {
           title="Información Básica"
           indicator={<RiUser3Fill className="text-slate-900 text-xl" />}
         >
-          {Object.keys(cvInfo.basicInfo).map((info) => {
-            const property = cvInfo.basicInfo[info];
-            // console.log({ info, property });
-            return (
-              <Input
-                key={info}
-                type={info === "email" ? "email" : "text"}
-                radius="sm"
-                label={property.label}
-                name={info}
-                value={property.value}
-                onChange={onCvChange}
-                className="text-slate-700 mb-2"
-              />
-            );
-          })}
+          {basicInfoInputElements}
         </AccordionItem>
-        <AccordionItem
-          key="2"
-          aria-label="Accordion 2"
-          title="Accordion 2"
-        ></AccordionItem>
-        <AccordionItem
-          key="3"
-          aria-label="Accordion 3"
-          title="Accordion 3"
-        ></AccordionItem>
+        {sectionAccordionElements}
       </Accordion>
-
-      <div>
-        <h1 className="text-lg font-semibold">Secciones:</h1>
-        {cvInfo.sections.map((section) => (
-          <p key={section.title}>{section.title}</p>
-          // Here it should also show the added details on each section with buttons to make it editable.
-          // Maybe just make them input
-        ))}
-      </div>
-
-      <Tooltip content="Add a section for testing." closeDelay={100}>
-        <Button
-          size="md"
-          radius="md"
-          color="primary"
-          variant="ghost"
-          onClick={() => createSection("Cute Section")}
-        >
-          TEST add new section
-        </Button>
-      </Tooltip>
     </nav>
   );
 }
